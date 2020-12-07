6f5519bb9aad361b5b398ff81f0677bc421e9f1e
Opilane TTHK <opilane@tthk.ee>
2020-12-07 14:56:31 +0200

    poolik

---
+++ OOPListsAndObjects/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPListsAndObjects
+{
+    class Program
+    {
+        class Planet
+        {
+            string name;
+            int mass;
+        
+            public Planet(string _name, int _mass)
+            {
+                name = _name;
+                mass = _mass;
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public int Mass
+            {
+                get { return mass; }
+            }
+        }
+
+        class ListOfPlanets
+        {
+            List<Planet> planets;
+            int totalMass;
+
+            public ListOfPlanets()
+            {
+                planets = new List<Planet>();
+                totalMass = 0;
+            }
+
+            public void AddPlanetToList(string planetName, int planetMass)
+            {
+                Planet newPlanet = new Planet(planetName, planetMass);
+                planets.Add(newPlanet);
+            }
+            
+            public void PrintPlanets()
+            {
+                foreach(Planet planetFromList in planets)
+                {
+                    Console.WriteLine("Planet name: " + planetFromList.Name + " | Mass: " + planetFromList.Mass);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
